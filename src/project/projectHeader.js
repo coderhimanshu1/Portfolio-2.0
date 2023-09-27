2ddb15da56e5c25fcd9dc3fac42277336c2a75ef
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-09-26 22:24:50 -0700

    finalizing responsiveness for project header

--- src/project/projectHeader.js
+++ src/project/projectHeader.js
@@ -1,5 +1,6 @@
 import "../static/styles/project/projectHeader.css";
 import Links from "./links";
+import Title from "../common/title";
 
 const ProjectHeader = ({
   projectName,
@@ -15,45 +16,57 @@ const ProjectHeader = ({
   handleButtonClick,
 }) => {
   return (
-    <div className="ProjectHeader" style={{ backgroundColor: backgroundColor }}>
-      <h1>{projectName}</h1>
-      <p>{description}</p>
-      <div className="ProjectHeader-info">
-        <div>
-          <h4>Roles</h4>
-          <p>{role}</p>
+    <>
+      <Title />
+      <div
+        className="ProjectHeader"
+        style={{ backgroundColor: backgroundColor }}
+      >
+        <h1>{projectName}</h1>
+        <div className="ProjectHeader-items">
+          <div className="ProjectHeader-items-text">
+            <p>{description}</p>
+            <div className="ProjectHeader-info">
+              <div>
+                <h4>Roles</h4>
+                <p>{role}</p>
+              </div>
+              <div>
+                <h4>Technologies</h4>
+                <p>{techStack}</p>
+              </div>
+              <div>
+                <h4>Timeline</h4>
+                <p>{timeline}</p>
+              </div>
+            </div>
+          </div>
+          <div className="ProjectHeader-items-demo">
+            {demoVideo && (
+              <div>
+                <iframe
+                  width="560"
+                  height="315"
+                  src={demoVideo}
+                  title="YouTube video player"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowFullScreen
+                  autoPlay
+                ></iframe>
+              </div>
+            )}
+            <Links github={github} liveLink={liveLink} API={API} />
+          </div>
         </div>
-        <div>
-          <h4>Technologies</h4>
-          <p>{techStack}</p>
-        </div>
-        <div>
-          <h4>Timeline</h4>
-          <p>{timeline}</p>
+        <div className="button-container">
+          <button className="Home-button" onClick={handleButtonClick}>
+            Find more...
+            <span className="arrow"></span>
+          </button>
         </div>
       </div>
-      {demoVideo && (
-        <div>
-          <iframe
-            width="560"
-            height="315"
-            src={demoVideo}
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
-            autoPlay
-          ></iframe>
-        </div>
-      )}
-      <Links github={github} liveLink={liveLink} API={API} />
-      <div className="button-container">
-        <button className="Home-button" onClick={handleButtonClick}>
-          Find more...
-          <span className="arrow"></span>
-        </button>
-      </div>
-    </div>
+    </>
   );
 };
 
