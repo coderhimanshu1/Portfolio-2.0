7aaacaaeec81a41e1d147b0bdef290dc11f5cc98
himanshukumar30 <himanshuk.kumar01@gmail.com>
2024-01-24 12:22:35 -0800

    adding tagline

--- src/homepage/header.js
+++ src/homepage/header.js
@@ -13,7 +13,7 @@ const Header = ({ handleButtonClick }) => {
 
   return (
     <div className="Header">
-      <Title />
+      <Title titleText="Making an Impact - One Step at a Time!" />
       <div className="Header-components">
         <div className="Header-profile">
           <h1 className="name">Hey! I'm Himanshu Kumar</h1>
